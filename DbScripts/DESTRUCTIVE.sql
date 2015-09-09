6e47532543d9fc467dfb450e8c48b452b0f7ef63
alton.crossley <alton.crossley@simmsfishing.com>
2015-09-09 09:53:58 -0600

    adding db scripts for issue #1

---
+++ DbScripts/DESTRUCTIVE.sql
@@ -0,0 +1,32 @@
+﻿USE [Logging]
+GO
+
+IF EXISTS 
+        (SELECT 
+             TABLE_NAME 
+         FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'logger_entries') 
+DROP TABLE [dbo].[logger_entries];
+
+IF EXISTS 
+        (SELECT 
+             TABLE_NAME 
+         FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'logger_operations') 
+DROP TABLE [dbo].[logger_operations];
+
+IF EXISTS 
+        (SELECT 
+             TABLE_NAME 
+         FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'logger_threads') 
+DROP TABLE [dbo].[logger_threads];
+
+IF EXISTS 
+        (SELECT 
+             TABLE_NAME 
+         FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'logger_thread_statuses') 
+DROP TABLE [dbo].[logger_thread_statuses];
+
+IF EXISTS 
+        (SELECT 
+             TABLE_NAME 
+         FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '__MigrationHistory') 
+DROP TABLE [dbo].[__MigrationHistory];
